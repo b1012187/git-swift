edf00cb17ad202864c4f8a75704b670a795977ac
諸原聖 <satoshi@STB1012187.local>
2015-04-28 12:41:43 +0900

    Initial Commit

---
+++ git-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-swift
+//
+//  Created by 諸原聖 on 2015/04/28.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
